9ca994e28cbfc6b7a29456a64ed7fe7b952dffbc
PhilippovPavel <philippov.artist.pavel@gmail.com>
2025-05-13 15:25:36 +0300

    faq save fix

--- build/faq/index.asset.php
+++ build/faq/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '59c539ad899c473a51de');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '9f991d969f29de2c9bc6');
--- src/faq/save.js
+++ src/faq/save.js
@@ -1,27 +1,31 @@
-import { useBlockProps } from '@wordpress/block-editor';
-
+import { useBlockProps, RichText } from '@wordpress/block-editor';
 
 export default function save({ attributes }) {
 	const { faq_block } = attributes;
 	return (
 		<div {...useBlockProps.save({ className: 'sd-question__details' })}>
 			<div className="sd-question__details">
-					<>
-						{faq_block.map((faq_item, index) => (
-							<div key={index} className="b-protocol__details">
-								{faq_item?.title && (
-									<h4 className="b-protocol__details-button">{faq_item.title}</h4>
-								)}
-								{faq_item?.description && (
-									<>
-										<div className="b-protocol__details-box">
-											<p>{faq_item.description}</p>
-										</div>
-									</>
-								)}
-							</div>
-						))}
-					</>
+				<>
+					{faq_block.map((faq_item, index) => (
+						<div key={index} className="b-protocol__details">
+							{faq_item?.title && (
+								<RichText.Content
+									tagName='h4'
+									className="b-protocol__details-button"
+									value={faq_item.title}
+								/>
+							)}
+							{faq_item?.description && (
+								<div className="b-protocol__details-box">
+									<RichText.Content
+										tagName='p'
+										value={faq_item.description}
+									/>
+								</div>
+							)}
+						</div>
+					))}
+				</>
 			</div>
 		</div>
 	);
